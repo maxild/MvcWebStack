f1eba7c09cdaf7ac5c4a17ba5cead352e4d86915
maxild <mmaxild@gmail.com>
2010-05-18 21:54:38 +0000

    Investigation through tests/specs of reflection against private properties and read-only properties running in full trust or partial trust

--- src/test/Maxfire.Skat.UnitTests/AccessorTester.cs
+++ src/test/Maxfire.Skat.UnitTests/AccessorTester.cs
@@ -1,4 +1,5 @@
-using System.ComponentModel;
+using System;
+using System.Security.Permissions;
 using Maxfire.Core.Reflection;
 using Maxfire.TestCommons.AssertExtensions;
 using Xunit;
@@ -14,7 +15,7 @@ namespace Maxfire.Skat.UnitTests
 		}
 
 		[Fact]
-		public void UsingPropertyAccessor()
+		public void PropertyAccessorWorksOnMutableType()
 		{
 			var p = new Point { X = 10, Y = 20 };
 
@@ -31,22 +32,106 @@ namespace Maxfire.Skat.UnitTests
 			p.Y.ShouldEqual(24);
 		}
 
-		[Fact]
-		public void UsingTypeDescriptor()
+		/// <summary>
+		/// Compiler generated backing field, and private setter methods
+		/// </summary>
+		public class ImmutablePointWithPrivateSetter
 		{
-			var p = new Point { X = 10, Y = 20 };
+			public ImmutablePointWithPrivateSetter(int x, int y)
+			{
+				X = x;
+				Y = y;
+			}
 
-			var propertyDescriptorForX = TypeDescriptor.GetProperties(p)["X"];
-			var propertyDescriptorForY = TypeDescriptor.GetProperties(p)["Y"];
+			public int X { get; private set; }
+			public int Y { get; private set; }
+		}
 
-			propertyDescriptorForX.GetValue(p).ShouldEqual(10);
-			propertyDescriptorForY.GetValue(p).ShouldEqual(20);
-			
-			propertyDescriptorForX.SetValue(p, 12);
-			propertyDescriptorForY.SetValue(p, 24);
+		[Fact]
+		public void PropertyAccessorWorksOnImmutableTypeWithPrivateSetterWhenRunningInFullTrust()
+		{
+			var p = new ImmutablePointWithPrivateSetter(10, 20);
+
+			var propertyX = IntrospectionOf<ImmutablePointWithPrivateSetter>.GetAccessorFor(x => x.X);
+			var propertyY = IntrospectionOf<ImmutablePointWithPrivateSetter>.GetAccessorFor(x => x.Y);
+
+			propertyX.GetValue(p).ShouldEqual(10);
+			propertyY.GetValue(p).ShouldEqual(20);
+
+			propertyX.SetValue(p, 12);
+			propertyY.SetValue(p, 24);
 
 			p.X.ShouldEqual(12);
 			p.Y.ShouldEqual(24);
+		}
+
+		public class CallingPrivateSetter : MarshalByRefObject
+		{
+			public int SetAndGetX(int value)
+			{
+				var p = new ImmutablePointWithPrivateSetter(0, 0);
+
+				var propertyX = IntrospectionOf<ImmutablePointWithPrivateSetter>.GetAccessorFor(x => x.X);
+
+				propertyX.SetValue(p, value);
+
+				return propertyX.GetValue(p);
+			}
+		}
+
+		// NOTE: Since .NET 2 SP1, reflection is available in partial trust, and reflection will demand MemberAccess in this case!
+
+		[Fact]
+		public void PropertyAccessorThrowsMethodAccessExceptionWhenCallingPrivateSetterWhenRunningInPartialTrustWithoutMemberAccess()
+		{
+			PartialTrustContext.RunTest<CallingPrivateSetter>(
+				runner => Assert.Throws<MethodAccessException>(() => runner.SetAndGetX(12)),
+				permissions => permissions.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.NoFlags)));
+		}
+
+		[Fact]
+		public void PropertyAccessorWorksOnImmutableTypeWithPrivateSetterWhenRunningInPartialTrustWithReflectionPermissionFlagRestrictedMemberAccess()
+		{
+			PartialTrustContext.RunTest<CallingPrivateSetter>(
+				runner => runner.SetAndGetX(12).ShouldEqual(12),
+				permissions => permissions.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.RestrictedMemberAccess)));
+		}
+
+		[Fact]
+		public void PropertyAccessorWorksOnImmutableTypeWithPrivateSetterWhenRunningInPartialTrustWithReflectionPermissionFlagMemberAccess()
+		{
+			PartialTrustContext.RunTest<CallingPrivateSetter>(
+				runner => runner.SetAndGetX(12).ShouldEqual(12),
+				permissions => permissions.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.MemberAccess)));
+		}
+
+		/// <summary>
+		/// No setter methods
+		/// </summary>
+		public class ImmutablePointWithNoSetter
+		{
+			private readonly int _x;
+			private readonly int _y;
+			
+			public ImmutablePointWithNoSetter(int x, int y)
+			{
+				_x = x;
+				_y = y;
+			}
+
+			public int X { get { return _x; } }
+			public int Y { get { return _y; } }
+		}
+
+		[Fact]
+		public void UsingPropertyAccessorOnImmutableTypeWithNoSetter()
+		{
+			var p = new ImmutablePointWithNoSetter(10, 20);
+
+			var propertyX = IntrospectionOf<ImmutablePointWithNoSetter>.GetAccessorFor(x => x.X);
+			
+			var ex = Assert.Throws<ArgumentException>(() => propertyX.SetValue(p, 12));
+			ex.Message.ShouldEqual("Property set method not found.");
 		}
 	}
 }
---
+++ src/test/Maxfire.Skat.UnitTests/PartialTrustContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+
+namespace Maxfire.Skat.UnitTests
+{
+	public static class PartialTrustContext
+	{
+		public static void RunTest<TRunner>(Action<TRunner> testDriver, Action<PermissionSet> permissionsSetup = null)
+		{
+			var setup = new AppDomainSetup
+			{
+				ApplicationBase = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+			};
+			
+			var permissions = new PermissionSet(null);
+			permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+			if (permissionsSetup != null)
+			{
+				permissionsSetup(permissions);
+			}
+			
+			AppDomain sandbox = null;
+			try
+			{
+				sandbox = AppDomain.CreateDomain("sandbox", null, setup, permissions);
+				var runner = (TRunner)sandbox.CreateInstanceAndUnwrap(Assembly.GetExecutingAssembly().FullName,
+				                                                      typeof(TRunner).FullName);
+
+				testDriver(runner);
+			}
+			finally
+			{
+				if (sandbox != null)
+				{
+					AppDomain.Unload(sandbox);
+				}
+			}
+		}
+	}
+}
