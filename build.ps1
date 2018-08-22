a4d3263aa050f1d75c518c9a7886c6c51c5121c7
maxild <mmaxild@gmail.com>
2018-08-22 15:07:46 +0200

    Opdater build script til samme tooling som Borbas
    * vs2017,
    * PSAKE 4.7,
    * xunit 2.4,
    * GitVersion.CommandLine 4.0.0-beta0012

--- build.ps1
+++ build.ps1
@@ -4,36 +4,52 @@ param (
     [ValidateSet("debug", "release")]
     [Alias('config')]
     [string]$Configuration = 'Release',
+    [Alias('h')]
+    [switch]$ShowHelp,
+    [Alias('t')]
+    [switch]$ShowTargets,
     [switch]$SkipRestore,
     [switch]$CleanCache,
-    [switch]$SkipTests
+    [switch]$SkipTests,
+    [switch]$UpdateNuget
 )
 
 $RepoRoot = $PSScriptRoot
-$NuGetExe = Join-Path $RepoRoot '.nuget\nuget.exe'
+$ToolsDir = Join-Path $RepoRoot 'tools'
+$NuGetExe = Join-Path $ToolsDir 'nuget.exe'
+
+if (-not (Test-Path $ToolsDir)) {
+    New-Item -ItemType directory -Path $ToolsDir -ErrorAction SilentlyContinue | out-null
+}
 
 function Install-NuGet {
     [CmdletBinding()]
     param([string] $NugetVersion = "latest")
     if (-not (Test-Path $NuGetExe)) {
         Write-Host -ForegroundColor Cyan 'Downloading nuget.exe'
-        $NuGetDir = Join-Path $RepoRoot '.nuget'
-        if (-not (Test-Path $NuGetDir)) {
-            New-Item -ItemType directory -Path $NuGetDir
-        }
         Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/$NugetVersion/nuget.exe" -OutFile $NuGetExe
+    }
+    elseif ($UpdateNuget.IsPresent) {
+        # Update Nuget client
+        & $NuGetExe update -self
     }
 }
 
 function Install-PSake {
-    if (-not (Test-Path (Join-Path $RepoRoot 'packages\psake'))) {
-        & $NuGetExe install psake -version 4.6.0 -ExcludeVersion -o packages -nocache
+    if (-not (Test-Path (Join-Path $ToolsDir 'psake'))) {
+        & $NuGetExe install psake -version 4.7.0 -ExcludeVersion -OutputDirectory `"$ToolsDir`" -nocache -Source https://api.nuget.org/v3/index.json
     }
 }
 
 function Install-GitVersion {
-    if (-not (Test-Path (Join-Path $RepoRoot 'packages\GitVersion.CommandLine'))) {
-        & $NuGetExe install GitVersion.CommandLine -version 4.0.0-beta0007 -ExcludeVersion -o packages -nocache
+    if (-not (Test-Path (Join-Path $ToolsDir 'GitVersion.CommandLine'))) {
+        & $NuGetExe install GitVersion.CommandLine -version 4.0.0-beta0012 -ExcludeVersion -OutputDirectory `"$ToolsDir`" -nocache -Source https://api.nuget.org/v3/index.json
+    }
+}
+
+function Install-XunitCliRunner {
+    if (-not (Test-Path (Join-Path $ToolsDir 'xunit.runner.console'))) {
+        & $NuGetExe install xunit.runner.console -version 2.4.0 -ExcludeVersion -OutputDirectory `"$ToolsDir`" -nocache -Source https://api.nuget.org/v3/index.json
     }
 }
 
@@ -46,10 +62,23 @@ function Install-SourceLink {
 Install-NuGet
 Install-PSake
 Install-GitVersion
-Install-SourceLink
+Install-XunitCliRunner
 
-# right now it is hardcoded to full task
-.\packages\psake\tools\psake.ps1 .\psakefile.ps1 $Target -properties @{configuration=$Configuration}
+$PsakePath = "$ToolsDir\psake\tools\psake\psake.ps1"
+
+if (-not (Test-Path $PsakePath)) {
+    throw "PSAKE have not been installed."
+}
+
+if ($ShowHelp.IsPresent) {
+    & $PsakePath -help
+}
+elseif ($ShowTargets.IsPresent) {
+    & $PsakePath .\psakefile.ps1 -docs
+}
+else {
+    & $PsakePath .\psakefile.ps1 $Target -properties @{configuration=$Configuration}
+}
 
 # report success or failure
 if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }
--- psakefile.ps1
+++ psakefile.ps1
@@ -1,17 +1,17 @@
-Framework 4.5.2
+Framework "4.7.1" # MSBuild 15
 
 properties {
     $base_dir = resolve-path .
-    $build_dir = join-path $base_dir "build"
-    $artifacts_dir = join-path $base_dir "artifacts"
     $source_dir = join-path $base_dir "src"
     $test_dir = join-path $base_dir "test"
-    $packages_dir = join-path $base_dir "packages"
+    $tools_dir = join-path $base_dir "tools"
+    $nuspec_dir = join-path $base_dir "nuspec"
+    $artifacts_dir = join-path $base_dir "artifacts"
     $sln_file = "Maxfire.sln"
     $configuration = "Release"
     $commonAssemblyInfoPath = join-path $base_dir "src" | join-path -ChildPath "CommonAssemblyInfo.cs"
     $framework_dir = Get-FrameworkDirectory
-    $tools_version = "14.0" # MSBuild 14 == vs2015 == C#6
+    $tools_version = "15.0" # MSBuild 15 == vs2017 == C#7
 }
 
 task default -depends dev
@@ -20,31 +20,128 @@ task dev -depends compile, test     -description "developer build (before commit
 task local -depends dev, pack       -description "local full build (producing local nupkg's)"
 task all -depends dev, srcidx, pack -description "full build (producing source linked nupkg's)"
 
-task resolveVersions {
-    $output = & "$packages_dir\GitVersion.Commandline\tools\GitVersion.exe" /output json
+# TODO: kan slettes for public repo
+task repairGitRemoteOnAppVeyor {
+    if ($env:APPVEYOR -ne $NULL) {
+
+        # private repo need github credentials
+        $env:GITVERSION_REMOTE_USERNAME = "maxild"
+        $env:GITVERSION_REMOTE_PASSWORD = $env:github_password
+
+        # Running on AppVeyor, we have to patch private repos
+        #   GitVersion (i.e. libgit2) doesn't support SSH, so to avoid 'Unsupported URL protocol'
+        #   when GitVersion fetches from the remote, we have to convert to using HTTPS
+        #   See http://help.appveyor.com/discussions/kb/17-getting-gitversion-to-work-with-private-bitbucketgithub-repositories
+
+        $remoteUrl = & git remote get-url origin
+
+        $repoHasHttpsUrl = $false;
+
+        # AppVeyor uses https for public repos
+        $httpsMatch = [System.Text.RegularExpressions.Regex]::Match($remoteUrl, "^https://github.com\/(?<RepositoryOwner>[-\w]+)/(?<RepositoryName>[-\w]+)\.git$", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+        if ($httpsMatch.Success) {
+            $repoOwner = $httpsMatch.Groups["RepositoryOwner"].Value;
+            $repoName = $httpsMatch.Groups["RepositoryName"].Value;
+            $repoHasHttpsUrl = $true;
+            Write-Verbose "RepositoryOwner '$repoOwner' and RepositoryName '$repoName' resolved from origin remote url of the form 'https://github.com/$repoOwner/$repoName.git'."
+        }
+        else {
+            # AppVeyor uses ssh for private repos
+            $sshMatch = [System.Text.RegularExpressions.Regex]::Match($remoteUrl, "^git@github.com:(?<RepositoryOwner>[-\w]+)/(?<RepositoryName>[-\w]+)\.git$", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+            if ($sshMatch.Success) {
+                $repoOwner = $sshMatch.Groups["RepositoryOwner"].Value;
+                $repoName = $sshMatch.Groups["RepositoryName"].Value;
+                Write-Verbose "RepositoryOwner '$repoOwner' and RepositoryName '$repoName' resolved from origin remote url 'git@github.com:$repoOwner/$repoName.git'."
+            }
+            else {
+                throw "Unable to resolve RepositoryOwner and RepositoryName from remote url '{$remoteUrl}'"
+            }
+        }
+
+        # make libgit2 use https
+        if ($repoHasHttpsUrl -eq $false) {
+            &git remote set-url origin "https://github.com/$repoOwner/$repoName.git"
+        }
+    }
+}
+
+task resolveVersion -depends repairGitRemoteOnAppVeyor {
+
+    $global:buildVersion = "local"
+
+    if ($env:APPVEYOR -ne $NULL) {
+
+        # Running on AppVeyor, we have to patch/setup local tracking branches (TODO: env vars)
+        $output = & "$tools_dir\GitVersion.Commandline\tools\GitVersion.exe" /output buildserver
+        if ($LASTEXITCODE -ne 0) {
+            if ($output -is [array]) {
+                Write-Error ($output -join "`r`n")
+            } else {
+                Write-Error $output
+            }
+            throw "GitVersion Exit Code: $LASTEXITCODE"
+        }
+
+        $global:majorMinorPatch = $env:GitVersion_MajorMinorPatch
+        $global:semVersion = $env:GitVersion_SemVer
+        $global:pkgVersion = $env:GitVersion_NuGetVersion
+        $global:infoVersion = $env:GitVersion_InformationalVersion
+        $global:buildVersion = "$($env:GitVersion_FullSemVer).build.$env:APPVEYOR_BUILD_NUMBER"
+
+        # Update appveyor build details (-Version must be unique)
+        Update-AppveyorBuild -Version $buildVersion
+    }
+
+    $output = & "$tools_dir\GitVersion.Commandline\tools\GitVersion.exe" /output json
     if ($LASTEXITCODE -ne 0) {
+        if ($output -is [array]) {
+            Write-Error ($output -join "`r`n")
+        } else {
+            Write-Error $output
+        }
         throw "GitVersion Exit Code: $LASTEXITCODE"
     }
     $versionInfoJson = $output -join "`n"
     $versionInfo = $versionInfoJson | ConvertFrom-Json
 
-    $global:pkgVersion = $versionInfo.LegacySemVerPadded
-    $global:assemblyVersion = $versionInfo.MajorMinorPatch
-    $global:assemblyFileVersion = ($assemblyVersion + ".0")
-    $global:assemblyInformationalVersion = $versionInfo.InformationalVersion    
+    # Git (version) info
+    $global:majorMinorPatch = $versionInfo.MajorMinorPatch
+    $global:semVersion = $versionInfo.SemVer
+    $global:pkgVersion = $versionInfo.NuGetVersion
+    $global:infoVersion = $versionInfo.InformationalVersion
 
-    # Update appveyor build details (-Version must be unique)
-    if ($env:APPVEYOR -ne $NULL) {
-        # TODO: Dette skal forbedres (appending to metadata)
-        Update-AppveyorBuild -Version "$($versionInfo.FullSemVer).build.$env:APPVEYOR_BUILD_NUMBER" 
-    }                
+    #$global:commitId   = $versionInfo.Sha
+    $global:commitId   = &git rev-parse --verify HEAD           # full 40 char Sha
+    #$global:commitId   = &git rev-parse --verify --short HEAD   # 7 char commit id
+
+    #$global:branchName = $versionInfo.BranchName
+    $global:branchName = & git rev-parse --verify --abbrev-ref HEAD
+
+    #$global:commitDate = $versionInfo.CommitDate
+
+    # 2016-09-26T14:59:32+02:00 (sidste linje er stdout)
+    $output = & git rev-list --format=%ad --date=iso-strict --max-count=1 HEAD
+    $commitDateAsIsoWithOffset = $output.Split([System.Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries) | Select-Object -Last 1
+    $commitDateTimeOffset = [System.DateTimeOffset]::ParseExact($commitDateAsIsoWithOffset, "yyyy-MM-dd'T'HH:mm:ss.FFFK", [System.Globalization.CultureInfo]::InvariantCulture)
+    $daDkTimeZoneId = "Romance Standard Time"
+    $commitDateTimeLocalDanishTime = [System.TimeZoneInfo]::ConvertTimeFromUtc($commitDateTimeOffset.UtcDateTime, [System.TimeZoneInfo]::FindSystemTimeZoneById($daDkTimeZoneId))
+    $global:commitDate = $commitDateTimeLocalDanishTime.ToString("yyyy-MM-dd'T'HH:mm:ss")
+
+    # Assembly (version) info
+    $global:assemblyVersion = $majorMinorPatch
+    $global:assemblyFileVersion = ($assemblyVersion + ".0")
+    $global:assemblyInformationalVersion = $infoVersion
 }
 
-task versionInfo -depends resolveVersions {
-    Write-Host "Version: $pkgVersion"
-    Write-Host "AssemblyVersion: $assemblyVersion"
-    Write-Host "AssemblyFileVersion: $assemblyFileVersion"
-    Write-Host "AssemblyInformationalVersion: $assemblyInformationalVersion"
+task verifyGit {
+    $gitFoundInPath = Get-File-Exists-On-Path "git.exe"
+    Assert $gitFoundInPath "Git executable cannot be found in PATH environment variable."
+    $gitVersion = &{git --version}
+    Write-Host "$gitVersion is installed and found in PATH environment variable."
+}
+
+task verifyVersion -depends resolveVersion {
+    Show-Configuration
 }
 
 # Source index the pdb file (See also http://ctaggart.github.io/SourceLink/exe.html)
@@ -63,53 +160,69 @@ task srcidx -depends compile {
         }
 
         # Traverse all project files (ie. packages)
-        $packages = Get-ChildItem $build_dir *.nuspec -recurse
+        $packages = Get-ChildItem $nuspec_dir *.nuspec -recurse
         $packages | %{
             $projectName = [io.path]::GetFileNameWithoutExtension($_.FullName)
             Write-Host "Source indexing PDB's from project $projectName" -ForegroundColor Yellow
             $projectfile   = "$source_dir\$projectName\$projectName.csproj"
-            
+
             # URL for downloading the source files, use {0} for commit and %var2% for path
             exec {
-                &"$packages_dir\SourceLink\tools\SourceLink.exe" index -pr $projectfile -pp Configuration $configuration -u 'https://raw.githubusercontent.com/maxild/MvcWebStack/{0}/%var2%' -c $commitId
+                &"$tools_dir\SourceLink\tools\SourceLink.exe" index -pr $projectfile -pp Configuration $configuration -u 'https://raw.githubusercontent.com/maxild/MvcWebStack/{0}/%var2%' -c $commitId
             }
 
         }
-        
+
     }
 }
 
-task Clean {
+task clean {
     delete_directory $artifacts_dir
 }
 
 task restore {
     exec {
-        # always update nuget to latest release
-        & $base_dir\.nuget\Nuget.exe update -self
-        & $base_dir\.nuget\Nuget.exe restore $sln_file
+        & $tools_dir\nuget.exe restore $sln_file
     }
 }
 
-task compile -depends clean, restore, commonAssemblyInfo {
-    Write-Host "Compiling '$sln_file' with '$configuration' configuration" -ForegroundColor Yellow
-    exec { msbuild /t:Clean /t:Build /p:Configuration=$configuration /p:TreatWarningsAsErrors=true /v:minimal /tv:${tools_version} /p:VisualStudioVersion=${tools_version} /maxcpucount "$sln_file" }
+task compile -depends restore, resolveVersion, commonAssemblyInfo {
+    Show-Configuration
+    exec {
+        # $output = &msbuild /version /nologo | Out-String
+        # Write-Host $output
+
+        #/p:TargetFrameworkVersion='v4.5.2' `
+        msbuild /t:Clean /t:Build `
+            /p:Configuration=$configuration `
+            /v:minimal `
+            /p:nowarn="1591" `
+            /tv:"$tools_version" `
+            /p:VisualStudioVersion="$tools_version" `
+            /p:TreatWarningsAsErrors='true' `
+            /p:MvcBuildViews='false' `
+            /p:MvcPublishWebsite='false' `
+            /maxcpucount `
+            "$sln_file"
+    }
 }
 
 task test -depends compile {
 
-    $xunitConsoleRunner = join-path $packages_dir 'xunit.runner.console.2.1.0' | `
+    # tfm is net452 (.NET Framework 4.5.2) (could be net461, net462, net471, net472)
+    $xunitConsoleRunner = join-path $tools_dir -ChildPath 'xunit.runner.console' | `
                           join-path -ChildPath 'tools' | `
+                          join-path -ChildPath 'net452' | `
                           join-path -ChildPath 'xunit.console.exe'
 
     $failures = @()
-        
+
     # Traverse all project files (ie. packages)
-    $packages = Get-ChildItem $build_dir *.nuspec -recurse
+    $packages = Get-ChildItem $nuspec_dir *.nuspec -recurse
     $packages | %{
         $testProjectName = ([io.path]::GetFileNameWithoutExtension($_.FullName) + ".UnitTests")
         $testAssemblyPath = "$test_dir\$testProjectName\bin\$configuration\$testProjectName.dll"
-        
+
         Write-Host "Executing tests from '$testProjectName' under configuration '$configuration'" -ForegroundColor Yellow
 
         & $xunitConsoleRunner $testAssemblyPath
@@ -123,7 +236,7 @@ task test -depends compile {
     }
 }
 
-task commonAssemblyInfo -depends resolveVersions {
+task commonAssemblyInfo -depends resolveVersion {
     $date = Get-Date
     "using System;
 using System.Reflection;
@@ -161,7 +274,7 @@ using System.Runtime.InteropServices;
 
 task pack -depends compile {
 
-    # ensure we have an artifacts dir 
+    # ensure we have an artifacts dir
     create_directory $artifacts_dir
 
     # Find dependency versions
@@ -175,7 +288,7 @@ task pack -depends compile {
 
     # Could use the -Version option of the nuget.exe pack command to provide the actual version.
     # _but_ the package dependency version cannot be overriden at the commandline.
-    $packages = Get-ChildItem $build_dir *.nuspec -recurse
+    $packages = Get-ChildItem $nuspec_dir *.nuspec -recurse
     $packages | %{
         $nuspec = [xml](Get-Content $_.FullName)
         $nuspec.package.metadata.version = $global:pkgVersion
@@ -209,13 +322,13 @@ task pack -depends compile {
         }
 
         $projectName = [io.path]::GetFileNameWithoutExtension($_.FullName)
-        
+
         $nuspecFilename = join-path $artifacts_dir (Split-Path -Path $_.FullName -Leaf)
         $nuspec.Save($nuspecFilename)
-        
+
         # All files in nuspec are relative paths, grab dll and pdb files using this base path
         $basePath = "$source_dir\$projectName\bin\$configuration"
-        
+
         exec { & $base_dir\.nuget\Nuget.exe pack -BasePath $basePath -OutputDirectory $artifacts_dir $nuspecFilename }
     }
 }
@@ -237,8 +350,44 @@ function find-dependencyVersion($packagesConfigPath, $packageId) {
 }
 
 # -------------------------------------------------------------------------------------------------------------
-# generalized functions
+# functions
 # --------------------------------------------------------------------------------------------------------------
+
+function Show-Configuration {
+    Write-Host -NoNewline "Compiling version '" -ForegroundColor Yellow
+    Write-Host -NoNewline "$semVersion" -ForegroundColor DarkGreen
+    Write-Host -NoNewline "' with '" -ForegroundColor Yellow
+    Write-Host -NoNewline "$configuration" -ForegroundColor DarkGreen
+    Write-Host "' configuration has resulted in versions defined by" -ForegroundColor Yellow
+
+    Write-Host -NoNewline "  Version: " -ForegroundColor Yellow
+    Write-Host "$semVersion" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  NuGetVersion: " -ForegroundColor Yellow
+    Write-Host "$pkgVersion" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  BuildVersion: " -ForegroundColor Yellow
+    Write-Host "$buildVersion" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  CommitId: " -ForegroundColor Yellow
+    Write-Host "$commitId" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  CommitDate: " -ForegroundColor Yellow
+    Write-Host "$commitDate" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  BranchName: " -ForegroundColor Yellow
+    Write-Host "$branchName" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  AssemblyVersion: " -ForegroundColor Yellow
+    Write-Host "$assemblyVersion" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  AssemblyFileVersion: " -ForegroundColor Yellow
+    Write-Host "$assemblyFileVersion" -ForegroundColor DarkGreen
+
+    Write-Host -NoNewline "  AssemblyInformationalVersion: " -ForegroundColor Yellow
+    Write-Host "$assemblyInformationalVersion" -ForegroundColor DarkGreen
+}
+
 function Get-File-Exists-On-Path([string]$file)
 {
     $results = ($env:Path).Split(";") | Get-ChildItem -filter $file -erroraction silentlycontinue
@@ -268,23 +417,6 @@ function Get-Git-Commit-Full
     else {
         return "0000000000000000000000000000000000000000"
     }
-}
-
-function Get-PackagePath {
-    param([string]$packageName)
-
-    $packagePath = Get-ChildItem "$packages_dir\$packageName.*" |
-                        Sort-Object Name -Descending |
-                        Select-Object -First 1
-    Return "$packagePath"
-}
-
-function Get-DependencyPackageFiles
-{
-    param([string]$packageName, [string]$frameworkVersion = "net45")
-
-    $packagePath = Get-PackagePath $packageName
-    Return "$packagePath\lib\$frameworkVersion\*"
 }
 
 # directory where MSBuild.exe is to be found
